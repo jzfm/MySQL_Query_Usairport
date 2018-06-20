19afeb4e7ae4cbbc25e366fa97e2c8ee139d1426
José Z. Farfus Medina <jzfm2009@gmail.com>
2018-06-20 12:06:40 +0200

    Altered 4 queries: 3 and 4) added month and year to ORDER BY for readability. 5) changed SUM function for COUNT for good practices when working with boolean datatypes, added a WHERE clause to just show results = 1 (TRUE). 7) Quering for highest distance total sorted by plane and not by carrier.

--- usairport.sql
+++ usairport.sql
@@ -11,17 +11,18 @@ GROUP BY Origin;
 SELECT origin AS Airport, colYear AS year1, colMonth AS month1, avg(ArrDelay) AS Delay 
 FROM flights 
 GROUP BY airport, month1, year1
-ORDER BY Airport;
+ORDER BY Airport, month1, year1;
 -- 4) Average arrival delay grouped by month and airport in the order AIRPORT/year/month/delay but airport must show the city name
 SELECT usairports.city AS city, colYear AS year1, colMonth AS month1, avg(ArrDelay) AS Delay 
 FROM flights 
 INNER JOIN usairports ON flights.Origin = usairports.IATA 
 GROUP BY city, month1, year1
-ORDER BY city;
+ORDER BY city, month1, year1;
 -- 5) The companies with the most cancel flights overall ordered from highest to lowest
-SELECT carriers.Description AS Company_name, SUM(flights.Cancelled) AS Cancel_flights 
+SELECT carriers.Description AS Company_name, COUNT(flights.Cancelled) AS Cancel_flights 
 FROM flights 
 INNER JOIN carriers ON flights.UniqueCarrier = carriers.CarrierCode
+WHERE flights.cancelled = 1
 GROUP BY Company_name 
 ORDER BY flights.Cancelled DESC;
 -- 6) Average delay from companies with arrival dealys over 10 minutes
@@ -31,7 +32,8 @@ INNER JOIN carriers ON flights.UniqueCarrier = carriers.CarrierCode
 GROUP BY company_name
 HAVING Average_delay > 10;
 -- 7) ID from the top 10 planes with most flight time
-SELECT carriers.CarrierCode AS carrier, flights.Distance AS miles
+SELECT carriers.CarrierCode AS carrier, SUM(flights.Distance) AS miles
 FROM flights 
 INNER JOIN carriers ON flights.UniqueCarrier = carriers.CarrierCode 
-ORDER BY flights.AirTime DESC limit 10;
+GROUP BY flights.TailNum
+ORDER BY miles DESC limit 10;
